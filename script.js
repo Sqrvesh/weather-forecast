f650d93dae21f624c8a8656cdcf53c0c5f13c817
Sqrvesh <sarveshhh42@gmail.com>
2024-12-13 17:33:51 +0530

    current info, daily info

---
+++ script.js
@@ -0,0 +1,103 @@
+async function loadWeatherData() {
+  const responseObj = await fetch("https://api.open-meteo.com/v1/forecast?latitude=13.0878&longitude=80.2785&current=temperature_2m,apparent_temperature,is_day,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_probability_max,wind_speed_10m_max,uv_index_max&timezone=auto&timeformat=unixtime");
+  const weatherDataRaw = await responseObj.json();
+  const current = weatherDataRaw.current;
+  const daily = weatherDataRaw.daily;
+
+  console.log(weatherDataRaw);
+
+  const weatherData = {
+    current: {
+      time: new Date(Number(current.time)* 1000),
+      temperature: current.temperature_2m,
+      apparentTemperature: current.apparent_temperature,
+      weatherCode: current.weather_code,
+      isDay: current.is_day,
+      windSpeed: current.wind_speed_10m
+    },
+
+    daily: {
+      weatherCode: daily.weather_code,
+      windSpeed: daily.wind_speed_10m_max,
+      temperatureMax: daily.temperature_2m_max,
+      temperatureMin: daily.temperature_2m_min,
+      time: daily.time.map((t) => new Date(Number(t)* 1000)),
+      precipitation: daily.precipitation_probability_max,
+      uv: daily.uv_index_max.map(index => findUVIndex(index))
+    }
+  };
+
+  console.log(weatherData);
+  return weatherData;
+  
+}
+
+async function loadWeatherDescriptions() {
+  const responseObj = await fetch("./data/weather-codes.json");
+  const weatherDescriptions = await responseObj.json();
+  return weatherDescriptions
+}
+
+function findUVIndex(index) {
+    if (Number(index) < 3) {
+      return 'Low'
+    } else if (3 <= Number(index) && Number(index) < 6) {
+      return 'Moderate'
+    } else if (6 <= Number(index) && Number(index) < 8) {
+      return 'High'
+    } else if (8 <= Number(index) && Number(index) < 11) {
+      return 'Very High'
+    } else if (Number(index) && Number(index) >= 11) {
+      return 'Extreme'
+    }
+}
+
+function genDailyWeatherCard(dailyData, weatherDescriptions) {
+  let html = '';
+  days = {0: 'Sunday', 1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday'};
+
+  dailyData.time.forEach( (date, index) => {
+    dateString = date.toDateString();
+    const weatherDescription = weatherDescriptions[Number(dailyData.weatherCode[index])]['day'];
+    html += `
+      <div class="daily-weather-card">
+        <div class="daily-date">
+          <div>${dateString.slice(8,11)} ${dateString.slice(4,8)}</div>
+          <div>${days[date.getDay()]}</div>
+        </div>
+        <div><img src=${weatherDescription.image}></div>
+        <div>
+          <div class="daily-temp-max">${Math.round(dailyData.temperatureMax[index])}°C</div>
+          <div class="daily-temp-min">/ ${Math.round(dailyData.temperatureMin[index])}°C</div>
+        </div>
+        <div class="daily-info">
+          <div class="daily-wind-speed">Wind Speed: <span class="daily-info-value">${Math.round(dailyData.windSpeed[index])}km/h</span></div>
+          <div class="daily-precipitation">Precipitaiton: <span class="daily-info-value">${Math.round(dailyData.precipitation[index])}%</span></div>
+          <div>UV: <span class="daily-info-value">${dailyData.uv[index]}</span></div>
+        </div>
+      </div>
+    `;
+  });
+    
+  return html;
+  
+}
+
+async function displayContent() {
+  const weatherData = await loadWeatherData();
+  const current = weatherData.current;
+  const daily = weatherData.daily;
+  const weatherDescriptions = await loadWeatherDescriptions();
+  const dayNight = current.isDay ? 'day' : 'night';
+  const weatherDescription = weatherDescriptions[Number(current.weatherCode)][dayNight];
+  document.querySelector('.js-date').innerHTML = current.time.toDateString();
+  document.querySelector('.js-temp').innerHTML = `${current.temperature}&deg;C`;
+  document.querySelector('.js-feelslike-temp').innerHTML = `feels like ${current.apparentTemperature}&deg;C`;
+  document.querySelector('.js-weather-description').innerHTML = weatherDescription.description;
+  document.querySelector('.js-weather-image-div').innerHTML = `<img class="weather-image" src="${weatherDescription.image}">`;
+  document.querySelector('.js-wind-speed').innerHTML = `Wind Speed: ${current.windSpeed} kmph`
+  document.querySelector('.js-daily-weather-card-flex').innerHTML = genDailyWeatherCard(daily, weatherDescriptions);
+  console.log('eeee')
+}
+
+displayContent();
